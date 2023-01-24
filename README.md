54ca61b5ce2b3a3ab14a7ee4f7bfa6af0887fb44
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-01-24 16:25:38 -0500

    updating files with generated README

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Title
 
-dfgj
+README Generator
 
 ## License
 
-![GNU](https://img.shields.io/badge/License-GNU-brightgreen.svg)(https://opensource.org/licenses/GPL-3.0)
+[![license](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
 
-- sfh
+- To build a README file generator that creates a README file based on input the user enters when prompted by the generator.
 
 ## Table of Contents
 
@@ -17,46 +17,54 @@ dfgj
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
+- [Demo](#demo)
 - [Contributing](#contributing)
 - [Credits](#credits)
-- [Test instructions](#test-instructions)
-- [Link to project](#link-to-project)
+- [Reference](#reference)
+- [Test Instructions](#test-instructions)
+- [Completed Project Link](#completed-project-link)
 - [Questions](#questions)
 
 ## Installation
 
-- fgh
+- Please install Node.jsv16 and npm Inquirer 8.2.4
+- https://nodejs.org/en/
+- https://www.npmjs.com/package/inquirer
 
 ## Usage
 
-- dfg
+- To use the generator, run the index.js file by right clicking on the file and running it in the node terminal. When the generator runs it will ask you questions that it will enter into the README file that is generated.
 
 ## Demo
 
-- dfgh
+- ![Screenshot-1](https://user-images.githubusercontent.com/66844643/214399273-76b3b79e-d5ed-48c8-b1c6-1238d1c14c3e.png)
+- ![Screenshot-2](https://user-images.githubusercontent.com/66844643/214399297-a60197b2-d673-4646-a17e-3a8410045e57.png)
+- ![Screenshot-3](https://user-images.githubusercontent.com/66844643/214403526-d356514c-4759-4f1f-896d-273ccd6f46b2.png)
+- https://drive.google.com/file/d/1l0tjbCQBqWwKQ-buKpu6GffSrtrnGtXk/view
 
-## contributing
+## Contributing
 
-- dfh
+- No contributions.
 
-## credits
+## Credits
 
-- dfgh
+- No.
 
 ## Reference
 
--sfgh
+- https://github.com/tiffany-brand/professional-README-generator
 
-## Test instructions
+## Test Instructions
 
-- sdf
+- In order to test the generator, run the index.js file in the node terminal. If there are any issues, you will get an error indicator in the terminal.
 
-## Link to project
+## Completed Project Link
 
-- fgh
+- https://github.com/Mandarinfish1/README-Generator
 
 # Questions
 
-- If you have additional questions, I can be reached by email at: fgh.
-- Github username: dfgh
-- Github Profile link: dgh
+- If you have questions, I can be reached by:
+- Email @ ruizpeggys@yahoo.com
+- Github Username: Mandarinfish1
+- Github Profile Link: https://github.com/Mandarinfish1
--- index.js
+++ index.js
@@ -2,97 +2,133 @@
 const inquirer = require('inquirer');
 const { writeFile } = require('fs').promises;
 
-//const badge = require('./createBadge');
 
-const askUser = () => {
-  return inquirer.prompt ([
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the Title of your Project?',
+const licenseLinks = {
+  'GNU General Public License v3.0':'[![license](https://img.shields.io/badge/License-GNU-brightgreen.svg)](https://opensource.org/licenses/GPL-3.0)',
+  'MIT':'[![license](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)',
+  'BSD 2-Claus Simplified License':'[![License](https://img.shields.io/badge/License-BSD%202-brightgreen.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+  'BSD 3-Claus New or Revised License':'[![License](https://img.shields.io/badge/License-BSD%203-brightgreen.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+  'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  'GNU Affero General Public License v3.0':'[![License](https://img.shields.io/badge/License-GNU%20Affero%20General%20Public%20License%20v3.0-brightgreen.svg)](https://opensource.org/licenses/AGPL-3.0)',
+  'GNU General Public License v2.0':'[![License](https://img.shields.io/badge/License-GPL--2.0-brightgreen.svg)](https://opensource.org/licenses/GPL-2.0)',
+  'GNU Lesser General Public License v2.1':'[![License](https://img.shields.io/badge/License-GNU%20Lesser%20General%20Public%20License%20v2.1-brightgreen.svg)](https://opensource.org/licenses/LGPL-2.1)',
+  'Mozilla Public License 2.0':'[![License](https://img.shields.io/badge/License-MPL--2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  'The Unlicense':'[![License](https://img.shields.io/badge/License-Unlicensed-brightgreen.svg)](https://opensource.org/licenses/unlicense)',
+  };
+    
+
+function askUser() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the Title of your Project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What license does your project have?',
+      choices: [
+        'GNU General Public License v3.0',
+        'MIT',
+        'BSD 2-Claus Simplified License',
+        'BSD 3-Claus New or Revised License',
+        'Apache 2.0',
+        'GNU Affero General Public License v3.0',
+        'GNU General Public License v2.0',
+        'GNU Lesser General Public License v2.1',
+        'Mozilla Public License 2.0',
+        'The Unlicense'],
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your Project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'installLink1',
+      message: 'Do you need to add a link to help with installing and running your project?',
+    },
+    { type: 'input',
+      name: 'installationLink2',
+      message: 'Do you need to add a second link to help with intalling and running your project?',
   },
-  { 
-  type: 'list',
-  name: 'badge',
-  message: 'What license does your project have?',
-  choices: 
-['![GNU](https://img.shields.io/badge/License-GNU-brightgreen.svg)](https://opensource.org/licenses/GPL-3.0)',
-'![MIT](https://img.shields.io/ badge/License-MIT-brightgreen.svg)](https://opensource.org/licenses/MIT)'],
-//"['GNU General Public License v3.0',
-    //'BSD 2-Claus Simplified License',
-    //'BSD 3-Claus New or Revised License',
-    //'GNU Affero General Public License v3.0',
-    //'GNU General Public License v2.0',
-    //'GNU Lesser General Public License v2.1',
-    //'Mozilla Public License 2.0',
-   // 'The Unlicense'],
-},
-  {
-    type: 'input',
-    name: 'description',
-    message: 'What is the description of your Project?',
-  },
-  { 
-   type: 'input',
-   name: 'installation',
-   message: 'What are the installation instructions for your project?',
-  },
-  {
-   type: 'input',
-   name: 'usage',
-   message:'What is the usage information for your project?',
-  },
-  { 
-  type: 'input',
-  name: 'demo',
-  message:  'Is there a link to a Demo?',
-  },
-  {
-  type: 'input',
-  name: 'contributing', 
-  message: 'What are the contribution guidelines for your project?',
- },
- {
-  type: 'input',
-  name: 'credit',
-  message: 'Are there any collaborators for your project?',
- },
- {
- type: 'input',
- name: 'reference',
- message: 'Are there any reference materials or reference links you used for your project?',
- },
- {
-  type: 'input',
-  name: 'test',
-  message: 'What are the test instructions for your project?',
-},
-{
-  type: 'input',
-  name: 'username',
-  message: 'What is your Github username?',
-},
-{ 
-  type: 'input',
-  name: 'email',
-  message:  'What is your email address?',
-  },
-  { 
-  type: 'input',
-  name: 'url',
-  message:  'Link to README Github Repository?',
-  },
-  { 
-  type: 'input',
-  name: 'profile',
-  message:  'What is your Github Profile link?',
-  },
- ]);
-};
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information for your project?',
+    },
+    {
+      type: 'input',
+      name: 'screenshot1',
+      message: 'Is there a screenshot to demo? Please add here:',
+    },
+    {
+      type: 'input',
+      name: 'screenshot2',
+      message: 'Is there another screenshot to demo? Plese add here:',
+    },
+    {
+      type: 'input',
+      name: 'screenshot3',
+      message: 'Do you need to add a screenshot of the completed Project? Please add here:',
+    },
+    {
+      type: 'input',
+      name: 'video',
+      message: 'Is there video you would like to add? Please add here:',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines for your project?',
+    },
+    {
+      type: 'input',
+      name: 'credit',
+      message: 'Are there any collaborators for your project?',
+    },
+    {
+      type: 'input',
+      name: 'reference',
+      message: 'Are there any reference materials or reference links you used for your project?',
+    },
+    {
+      type: 'input',
+      name: 'url',
+      message: 'Link to README Github Repository?',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'What are the test instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?',
+    },
+    {
+      type: 'input',
+      name: 'profile',
+      message: 'What is your Github Profile link?',
+    },
+  ]);
+}
  
 
-
-const generateReadMe = ({ title, badge, description, installation, usage, contributing, credit, reference, test, demo, url, email, username, profile,  }) =>
+const generateReadMe = ({ title, license,description,  installation, installLink1, installLink2, usage, screenshot1, screenshot2, screenshot3, video,contributing, credit, reference, url, test, email, username,  profile,   }) =>
 
 
 `# Title
@@ -101,7 +137,8 @@ ${title}
 
 ## License
 
-${badge}
+${licenseLinks[license]}
+
 
 ## Description
 
@@ -113,15 +150,19 @@ ${badge}
 - [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
+- [Demo](#demo)
 - [Contributing](#contributing)
 - [Credits](#credits)
-- [Test instructions](#test-instructions)
-- [Link to project](#link-to-project)
+- [Reference](#reference)
+- [Test Instructions](#test-instructions)
+- [Completed Project Link](#completed-project-link)
 - [Questions](#questions)
 
 ## Installation
 
 - ${installation}
+- ${installLink1}
+- ${installLink2}
 
 ## Usage
 
@@ -129,35 +170,38 @@ ${badge}
 
 ## Demo
 
-- ${demo}
+- ${screenshot1}
+- ${screenshot2}
+- ${screenshot3}
+- ${video}
 
-
-## contributing
+## Contributing
 
 - ${contributing}
 
-## credits
+## Credits
 
 - ${credit}
 
 ## Reference
 
--${reference}
+- ${reference}
 
-
-## Test instructions
+## Test Instructions
 
 - ${test}
 
-## Link to project
+## Completed Project Link
 
 - ${url}
 
 #  Questions
 
-- If you have additional questions, I can be reached by email at: ${email}.
-- Github username: ${username}
-- Github Profile link: ${profile}`
+- If you have questions, I can be reached by:
+- Email @ ${email}
+- Github Username: ${username}
+- Github Profile Link: ${profile}`;
+
 
 
 const init = () => {
@@ -166,6 +210,8 @@ const init = () => {
     .then(() => console.log('README.md file has been created!'))
     .catch((err) => console.error(err));
 }
+
+
 
 init();
 
